5cbbf994df2839ced1dee797209de83e69662601
Parth Patel <patelsparth00@gmail.com>
2025-02-01 18:31:39 -0800

    Fixed spacing of navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -8,19 +8,25 @@ const Navbar = () => {
   return (
     <nav>
       <div className="p-4 bg-blue-600 text-white">
-        {/* Logo/Name of the website. Clicking this should route
+        <div className="flex justify-between items-center max-w-6xl mx-auto">
+          {/* Logo/Name of the website. Clicking this should route
         user to the home page*/}
-        <Link to="/" className="text-2xl font-bold">
-          QuizMaster
-        </Link>
-        {/* Menu button for mobile */}
-        <button className="lg:hidden" onClick={() => setIsOpen(!isOpen)}>
-          {isOpen ? (
-            <XMarkIcon className="h-6 w-6 text-gray-800" />
-          ) : (
-            <Bars3Icon className="h-6 w-6 text-gray-800" />
-          )}
-        </button>
+          <Link to="/" className="text-2xl font-bold">
+            QuizMaster
+          </Link>
+          {/* Menu button for that should only show for tablet and smaller devices*/}
+          <button
+            className="lg:hidden"
+            onClick={() => setIsOpen(!isOpen)}
+            aria-label="Toggle navbar"
+          >
+            {isOpen ? (
+              <XMarkIcon className="h-6 w-6 text-white-800" />
+            ) : (
+              <Bars3Icon className="h-6 w-6 text-white-800" />
+            )}
+          </button>
+        </div>
       </div>
     </nav>
   );
