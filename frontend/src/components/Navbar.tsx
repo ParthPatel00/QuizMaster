06a30b5ac277e87e0c7e9630848dacbcc602f3b0
Parth Patel <patelsparth00@gmail.com>
2025-02-01 18:23:37 -0800

    Added heroicons for icons Trying to create a responsive navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { XMarkIcon, Bars3Icon } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
+  // For toggling menu bar
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <nav>
       <div className="p-4 bg-blue-600 text-white">
+        {/* Logo/Name of the website. Clicking this should route
+        user to the home page*/}
         <Link to="/" className="text-2xl font-bold">
           QuizMaster
         </Link>
+        {/* Menu button for mobile */}
+        <button className="lg:hidden" onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? (
+            <XMarkIcon className="h-6 w-6 text-gray-800" />
+          ) : (
+            <Bars3Icon className="h-6 w-6 text-gray-800" />
+          )}
+        </button>
       </div>
     </nav>
   );
