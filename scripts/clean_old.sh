7a7a6f77871c31ace04a26668a53b18767c30c3d
Dev Patel <61112662+devpateltech007@users.noreply.github.com>
2025-05-02 18:29:28 -0700

    Update clean_old.sh

--- scripts/clean_old.sh
+++ scripts/clean_old.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+set -e
+
 TARGET=/home/ubuntu/QuizMaster
+
 if [ -d "$TARGET" ]; then
-  rm -rf "${TARGET:?}/"*
+  # delete everything inside, including hidden files, without failing on empty
+  find "$TARGET" -mindepth 1 -delete
 fi
